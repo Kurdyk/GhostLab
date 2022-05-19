5629ac55b119e8ec09df42866e42043a7ec15ef8
Victor Josso <victor@josso.fr>
2022-05-19 14:33:04 +0200

    Debut de l'affichage du chat

---
+++ Client/src/utils/ChatListViewCell.java
@@ -0,0 +1,56 @@
+package utils;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+
+import java.io.IOException;
+
+/**
+ * The type Players list view cell.
+ */
+public class ChatListViewCell extends ListCell<ChatItem> {
+    @FXML
+    private Label fromAndToLabel;
+
+    @FXML
+    private Label messageLabel;
+
+    @FXML
+    private GridPane gridPane;
+
+    @FXML
+    private AnchorPane anchorPane;
+
+
+    private FXMLLoader mLLoader;
+
+    @Override
+    protected void updateItem(ChatItem chatItem, boolean empty) {
+        super.updateItem(chatItem, empty);
+
+        if (empty || chatItem == null){
+            setText(null);
+            setGraphic(null);
+        } else {
+            if (mLLoader == null){
+                mLLoader = new FXMLLoader(getClass().getResource("/views/chatListCell.fxml"));
+                mLLoader.setController(this);
+
+                try {
+                    mLLoader.load();
+                } catch (IOException e1){
+                    e1.printStackTrace();
+                }
+            }
+            fromAndToLabel.setText(chatItem.getFromAndTo());
+            messageLabel.setText(chatItem.getMessage());
+
+            setText(null);
+            setGraphic(anchorPane);
+        }
+    }
+}
